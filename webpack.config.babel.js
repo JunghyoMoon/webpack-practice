329f627a1790b25d62255dddaea661f5c8c9858b
junghyomoon <dsinhome70@gmail.com>
2021-03-24 11:26:48 +0900

    webpack setup is done. but there are some errors.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,21 +1,29 @@
 import webpack from "webpack";
 import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const ENTRY_POINT = path.resolve(__dirname, "src", "js", "main.js");
 const OUTPUT_POINT = path.resolve(__dirname, "build");
 
-export default {
-	mode: "none",
+const config = {
 	entry: [ENTRY_POINT],
 	output: {
 		path: OUTPUT_POINT,
-		filename: "app.bundle.js",
+		filename: "[name].bundle.js",
 	},
+	devServer: {
+		port: 4000,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.pug",
+		}),
+	],
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
-				use: [{ loader: "babel-loader" }],
+				test: /\.pug$/,
+				use: [{ loader: "pug-loader" }],
 			},
 			{
 				test: /\.(scss|sass)$/,
@@ -25,7 +33,19 @@ export default {
 					{ loader: "sass-loader" },
 				],
 			},
+			{
+				test: /\.js$/,
+				use: [{ loader: "babel-loader" }],
+			},
 		],
 	},
-	plugins: [],
+};
+
+export default (env, argv) => {
+	if (argv.mode === "development") {
+	}
+	if (argv.mode === "production") {
+	}
+
+	return config;
 };
